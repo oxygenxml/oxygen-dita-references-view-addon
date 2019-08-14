fbed1b498e60c68d3c4fb729e2efd1335188ce75
Alexandra Dinisor <alexandra_dinisor@yahoo.com>
2019-08-14 16:22:14 +0300

    removing some logging

--- src/main/java/com/oxygenxml/sdksamples/workspace/authorpage/AuthorReferencesCollector.java
+++ src/main/java/com/oxygenxml/sdksamples/workspace/authorpage/AuthorReferencesCollector.java
@@ -30,19 +30,7 @@ public class AuthorReferencesCollector extends ReferencesCollector {
 					.findNodesByXPath(ALL_REFS_XPATH_EXPRESSION, false, false, false);
 
 			// add all Author NodeRanges found by XPath evaluation
-			for (int i = 0; i < referenceAuthorNodeRanges.length; i++) {	
-//				System.err.println("__________________________________________");
-//				System.err.println(
-//						"getDisplayName(): " + referenceAuthorNodeRanges[i].getDisplayName() + " | getStartOffset(): "
-//								+ referenceAuthorNodeRanges[i].getStartOffset() + " | getEndOffset(): "
-//								+ referenceAuthorNodeRanges[i].getEndOffset());
-//				System.err.println(" ---> getAttributes: ");
-//				for (int j = 0; j < ((AuthorElement) referenceAuthorNodeRanges[i]).getAttributesCount(); j++) {
-//					String attributeName = ((AuthorElement) referenceAuthorNodeRanges[i]).getAttributeAtIndex(j);
-//					System.err.println("attributeName: " +  attributeName
-//							+ " | getValue(): " + ((AuthorElement) referenceAuthorNodeRanges[i]).getAttribute(attributeName).getValue());
-//				}
-
+			for (int i = 0; i < referenceAuthorNodeRanges.length; i++) {
 				NodeRange refRange = new AuthorPageNodeRange((AuthorElement) referenceAuthorNodeRanges[i]);
 				ranges.add(refRange);
 			}
