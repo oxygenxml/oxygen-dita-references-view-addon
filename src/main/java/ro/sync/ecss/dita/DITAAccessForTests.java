512ca7726d33ef756d3425e053010890901a569d
Alexandra Dinisor <alexandra_dinisor@yahoo.com>
2019-08-21 16:07:31 +0300

    Update ditamap with relTable

--- src/main/java/ro/sync/ecss/dita/DITAAccessForTests.java
+++ src/main/java/ro/sync/ecss/dita/DITAAccessForTests.java
@@ -1,10 +1,13 @@
 package ro.sync.ecss.dita;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.oxygenxml.ditareferences.workspace.rellinks.RelLink;
+import com.oxygenxml.ditareferences.workspace.rellinks.RelLinkImpl;
 
 public class DITAAccessForTests{
 	 /**
@@ -17,21 +20,21 @@ public class DITAAccessForTests{
 	   */
 	public static List<RelLink> getRelatedLinksFromReltable(URL originatorURL){
 		List<RelLink> links = new ArrayList<RelLink>();
-//		try {
-//			links.add(new RelLinkImpl(
-//					new File("test/source1.dita").toURI().toURL(), 
-//					new File("test/target1.dita").toURI().toURL(), null, null, new File("test/defLocation1.dita").toURI().toURL()));
-//			
-//			links.add(new RelLinkImpl(
-//					new File("test/source2.dita").toURI().toURL(), 
-//					new File("test/target2.dita").toURI().toURL(), null, null, new File("test/defLocation2.dita").toURI().toURL()));
-//			
-//			links.add(new RelLinkImpl(
-//					new File("test/source1.dita").toURI().toURL(), 
-//					new URL("http://www.google.com"), "html", "external", new File("test/defLocation3.dita").toURI().toURL()));
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			links.add(new RelLinkImpl(
+					new File("test/source1.dita").toURI().toURL(), 
+					new File("test/target1.dita").toURI().toURL(), null, null, new File("test/defLocation1.dita").toURI().toURL()));
+			
+			links.add(new RelLinkImpl(
+					new File("test/source2.dita").toURI().toURL(), 
+					new File("test/target2.dita").toURI().toURL(), null, null, new File("test/defLocation2.dita").toURI().toURL()));
+			
+			links.add(new RelLinkImpl(
+					new File("test/source1.dita").toURI().toURL(), 
+					new URL("http://www.google.com"), "html", "external", new File("test/defLocation3.dita").toURI().toURL()));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 		return links;
 	}
 }
