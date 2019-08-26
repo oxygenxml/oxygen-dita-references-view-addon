f9f81dfd99f3e6ce09370b294c35bb884f600a0f
Alexandra Dinisor <alexandra_dinisor@yahoo.com>
2019-08-26 12:43:23 +0300

    Fix for https://github.com/oxygenxml/oxygen-dita-outgoing-references-view/issues/21

--- src/main/java/com/oxygenxml/ditareferences/workspace/rellinks/RelLinksAccessor.java
+++ src/main/java/com/oxygenxml/ditareferences/workspace/rellinks/RelLinksAccessor.java
@@ -29,6 +29,7 @@ public class RelLinksAccessor {
 			Class ditaAccessClass = Class.forName("ro.sync.ecss.dita.DITAAccess" + (forTests ? "ForTests" : ""));
 			Method getRelLinks = ditaAccessClass.getDeclaredMethod("getRelatedLinksFromReltable", URL.class);
 			List allLinks = (List) getRelLinks.invoke(null, topicURL);
+
 			if (allLinks != null) {
 				int size = allLinks.size();
 				for (int i = 0; i < size; i++) {
@@ -36,8 +37,11 @@ public class RelLinksAccessor {
 					Class relLinkClass = relLink.getClass();
 					Method getSource = relLinkClass.getMethod("getSourceURL");
 					URL sourceURL = (URL) getSource.invoke(relLink);
-
-					if (topicURL.equals(sourceURL)) {
+					
+					String trimmedTopicURL = getURLWithoutAnchor(topicURL.toString());
+					String trimmedSourceURL = getURLWithoutAnchor(sourceURL.toString());
+															
+					if (trimmedTopicURL.equals(trimmedSourceURL)) {
 						Method getTarget = relLinkClass.getMethod("getTargetURL");
 						URL targetURL = (URL) getTarget.invoke(relLink);
 
@@ -61,6 +65,21 @@ public class RelLinksAccessor {
 		}
 
 		return links;
+	}
+
+	/**
+	 * Get the Topic URL String without its anchor if any or the whole URL if no
+	 * anchor found.
+	 * 
+	 * @param topicURLString The created Topic URL String
+	 * @return The Topic URL without the anchor if any.
+	 */
+	private static String getURLWithoutAnchor(String topicURLString) {
+		int indexOfAnchor = topicURLString.indexOf('#');
+		if(indexOfAnchor != -1) {
+			return topicURLString.substring(0, indexOfAnchor);
+		}
+		return topicURLString;
 	}
 
 }
