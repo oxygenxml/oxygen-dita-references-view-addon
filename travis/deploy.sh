d9bddf5e4c00c1c5a8310bfa08da68679e791ba9
AdrianSorop <adrian_sorop@sync.ro>
2022-01-26 17:32:51 +0200

    SPM-343 - sters scripturi de travis

--- travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-git config user.name "sorincarbunaru";
-git config user.email "sorin_carbunaru@sync.ro";
-git fetch;
-git checkout master;
-git reset;
-cp -f target/addon.xml build;
-git add build/addon.xml;
-git commit -m "New release - ${TRAVIS_TAG}";
-git push origin HEAD:master; 
